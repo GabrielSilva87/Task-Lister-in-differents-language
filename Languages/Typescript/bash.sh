19f7c81fe6920f724d6ba9ea9ea8c562ba09c096
Gabriel da Silva Gomes <gabri2012mi@gmail.com>
2025-03-31 23:47:23 -0300

    Criar bash.sh

---
+++ Languages/Typescript/bash.sh
@@ -0,0 +1,8 @@
+npm install electron --save-dev
+npm install typescript @types/node @types/electron --save-dev
+mkdir task_manager_electron
+cd task_manager_electron
+npm init -y
+mkdir src
+mkdir src/assets
+mkdir src/pages
