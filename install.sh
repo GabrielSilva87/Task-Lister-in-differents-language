b02a116f637f758f12490419984c10ddb6acb2bc
Gabriel da Silva Gomes <gabri2012mi@gmail.com>
2025-04-03 19:10:25 -0300

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ sudo ./linux-install.sh
 sudo apt install sbcl
 sudo apt install clisp
 sudo apt install dmd
+sudo apt update -y
+sudo apt install apt-transport-https
+sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'echo "deb [arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main" > /etc/apt/sources.list.d/dart_stable.list'
+sudo apt update
+sudo apt install dart
