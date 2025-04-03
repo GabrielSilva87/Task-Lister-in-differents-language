fbd9c914e8d67856b38c9abc5f73aad8fabec0ac
Gabriel da Silva Gomes <gabri2012mi@gmail.com>
2025-04-03 19:01:49 -0300

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 sudo apt-get install nasm
 sudo apt install libgtk-3-dev
+wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt update
+sudo apt install build-essential
